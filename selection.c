ecea77996a3820197769665ac085ae8f2f4ba9c5
Pradyumna SK <115913218+Pradyu12@users.noreply.github.com>
2024-05-21 14:50:24 +0530

    Add files via upload

---
+++ selection.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+    int arr[20], n, i, j, temp;
+    clrscr();
+    printf("Enter number of elements \n");
+    scanf("%d", &n);
+    printf("Enter elements of Array \n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Array after sorting\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    getch();
+}
