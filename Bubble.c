0a1f9c23168529c3bec3cb32bcbf7562058da30d
Pradyumna SK <115913218+Pradyu12@users.noreply.github.com>
2024-05-23 10:53:22 +0530

    Add files via upload

--- Bubble.c
+++ Bubble.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<conio.h>
-
 void main() {
     int arr[25], n, i, j, temp;
     clrscr();
-    printf("Enter number of elements \n");
+    printf("Enter size of array \n");
     scanf("%d", &n);
     printf("Enter elements of Array \n");
     for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    printf("Array after sorting\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    getch();
+    printf("array element before sort\n);
+    for (i = 0; i < n 1; i++)
+    printf("%d\n",a[i]);
+    for(i=0;i<n;i++)
+    {
+     for(j=0;j<n-1;j++)
+     {
+      if(a[i]<a[i])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("Array after sort \n");
+for(i=0;i<n;i++)
+printf("___%d\n",a[i]);
+getch();
 }
