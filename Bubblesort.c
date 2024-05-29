45c3db03d3c16c2d0ed926feb6468568cf6b473b
Pradyumna SK <115913218+Pradyu12@users.noreply.github.com>
2024-05-29 09:08:33 +0530

    Update Bubblesort.c

--- Bubblesort.c
+++ Bubblesort.c
@@ -9,7 +9,7 @@ void main() {
     for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
     printf("array element before sort\n");
-    for (i = 0; i < n 1; i++)
+    for (i = 0; i < n ; i++)
     printf("%d\n",a[i]);
     for(i=0;i<n;i++)
     {
